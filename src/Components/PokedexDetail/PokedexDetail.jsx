f47bd33dba9231aea34b36a0966ed8aadc2242a5
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-04-11 03:19:03 -0300

    Pokemon Detail Styles

--- src/Components/PokedexDetail/PokedexDetail.jsx
+++ src/Components/PokedexDetail/PokedexDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Route, Routes, useLocation, useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
@@ -7,27 +7,31 @@ import PokedexEvolution from '../PokedexEvolution/PokedexEvolution';
 import PokedexMoves from '../PokedexMoves/PokedexMoves';
 import PokedexStats from '../PokedexStats/PokedexStats';
 
-export default function PokedexDetail() {
-    let { name } = useParams()
+export default function PokedexDetail({ pokemon, specie, evolutions }) {
+    const params = useParams()
 
-  return (
-    <Wrap>
-        <Tabs>
-            <Tab><Link to={`about`}>About</Link></Tab>
-            <Tab><Link to={`stats`}>Base Stats</Link></Tab>
-            <Tab><Link to={`evolution`}>Evolution</Link></Tab>
-            <Tab><Link to={`moves`}>Moves</Link></Tab>
-        </Tabs>
-        <Content>
-            <Routes>
-                <Route path="/" element={<PokedexAbout/>}/>
-                <Route path="stats" element={<PokedexStats/>}/>
-                <Route path="evolution" element={<PokedexEvolution/>}/>
-                <Route path="moves" element={<PokedexMoves/>}/>
-            </Routes>
-        </Content>
-    </Wrap>
-  )
+    const checkTab = (currentTab) =>{
+        return currentTab == params['*'];
+    }
+
+    return (
+        <Wrap>
+            <Tabs>
+                <Tab active={checkTab('')}><Link to={``}>About</Link></Tab>
+                <Tab active={checkTab('stats')}><Link to={`stats`}>Base Stats</Link></Tab>
+                <Tab active={checkTab('evolution')}><Link to={`evolution`}>Evolution</Link></Tab>
+                <Tab active={checkTab(`moves`)}><Link to={`moves`}>Moves</Link></Tab>
+            </Tabs>
+            <Content>
+                <Routes>
+                    <Route path="/" element={<PokedexAbout pokemon={pokemon} specieDetail={specie}/>} />
+                    <Route path="stats" element={<PokedexStats stats={pokemon.stats}/>} />
+                    <Route path="evolution" element={<PokedexEvolution evolutions={evolutions}/>} />
+                    <Route path="moves" element={<PokedexMoves  moves={pokemon.moves}/>} />
+                </Routes>
+            </Content>
+        </Wrap>
+    )
 }
 
 
@@ -50,13 +54,15 @@ const Tab = styled.div`
     text-align: center;
     font-size: 1.1rem;
     font-weight: 600;
+    border-bottom: ${props => props.active ? '2px solid black' : ''};
 
     >a{
         text-decoration: none;
-        color: hsl(0deg 0% 61%);
+        color:${props => !props.active ? ' hsl(0deg 0% 61%) ' : 'black'};
     }
 `;
 
-const Content = styled.div``;
-
-const TabContent = styled.div``;
+const Content = styled.div`
+    margin: auto;
+    max-width: 820px;
+`;
--- src/Pages/PokemonDetail.jsx
+++ src/Pages/PokemonDetail.jsx
@@ -10,6 +10,8 @@ import PokedexDetail from '../Components/PokedexDetail/PokedexDetail';
 export default function PokemonDetail() {
     let { name } = useParams()
     const [pokemon, setPokemon] = useState();
+    const [specie, setSpecie] = useState()
+    const [evolutions, setEvolutions] = useState()
     const [color, setColor] = useState()
     const [loading, setLoading] = useState(true)
 
@@ -22,20 +24,37 @@ export default function PokemonDetail() {
         console.log(json)
     }
 
+    async function getSpecie() {
+        let data = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}/`)
+        let json = await data.json()
+        setSpecie(json)
+    }
+
+    async function getEvolutions() {
+        if (!specie) return;
+        let data = await fetch(specie?.evolution_chain.url)
+        let json = await data.json()
+        setEvolutions(json)
+    }
+
     useEffect(() => {
         getPokemon()
+        getSpecie()
     }, [name])
+    useEffect(() => {
+        getEvolutions()
+    }, [specie])
 
     return (
         <Container>
-            <PokedexHeader 
-            id={pokemon?.id} 
-            name={pokemon?.name}
-            color={color}
-            sprite={pokemon?.sprites?.other?.['official-artwork']?.front_default}
-            types={pokemon?.types}
-            speciesUrl={pokemon?.species?.url}/>
-            <PokedexDetail/>
+            <PokedexHeader
+                id={pokemon?.id}
+                name={pokemon?.name}
+                color={color}
+                sprite={pokemon?.sprites?.other?.['official-artwork']?.front_default}
+                types={pokemon?.types}
+                speciesUrl={pokemon?.species?.url} />
+            <PokedexDetail pokemon={pokemon} specie={specie} evolutions={evolutions} />
         </Container>
     )
 }
