58c2c8a5b092837153833fa820d52dbcf38662de
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-05-02 10:53:05 -0300

    add bars to stats, add regionText

--- src/Pages/Routing.jsx
+++ src/Pages/Routing.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route,  } from 'react-router-dom'
 import Pokedex from './Pokedex'
 import PokedexDetail from './PokedexDetail'
 
 export default function Main() {
   return (
-    <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basemname={`${process.env.PUBLIC_URL}`}>
         <Routes>
-            <Route path="/" element={<Pokedex/>}/>
+            <Route  path="/" element={<Pokedex/>}/>
             <Route path="/:name" element={<PokedexDetail/>}/>
         </Routes>
     </BrowserRouter>
