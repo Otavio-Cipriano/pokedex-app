cef7ea9a11463a9edbf3c8b9f9934454fa55e083
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-05-02 10:43:58 -0300

    packgage.json update

--- src/Pages/Routing.jsx
+++ src/Pages/Routing.jsx
@@ -5,7 +5,7 @@ import PokedexDetail from './PokedexDetail'
 
 export default function Main() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
         <Routes>
             <Route path="/" element={<Pokedex/>}/>
             <Route path="/:name" element={<PokedexDetail/>}/>
